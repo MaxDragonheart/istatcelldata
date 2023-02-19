9e8fae439af7523d48b3200df33f23021b2eddb5
MaxDragonheart <massimilianomoraca@gmail.com>
2023-02-19 11:02:48 +0100

    start development

--- census_istat/config.py
+++ census_istat/config.py
@@ -1,5 +1,4 @@
 import logging
-import os
 from pathlib import Path
 
 logger = logging.getLogger()
@@ -9,13 +8,4 @@ console_handler = logging.StreamHandler()
 console_handler.setFormatter(logging.Formatter(log_format))
 logger.addHandler(console_handler)
 
-MAIN_DATA_PATH = os.getenv("DATA_PATH")
-main_path = Path(MAIN_DATA_PATH)
-
-
-if __name__ == '__main__':
-    print(MAIN_DATA_PATH)
-    print(type(MAIN_DATA_PATH))
-
-    print(main_path)
-    print(type(main_path))
+main_path = Path("/home/max/Desktop/census_istat/")
