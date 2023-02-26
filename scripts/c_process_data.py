9c8a72eec32892dc4ff9a21c61c7d275043bcbe8
MaxDragonheart <massimilianomoraca@gmail.com>
2023-02-26 13:23:30 +0100

    minor fix

--- scripts/c_process_data.py
+++ scripts/c_process_data.py
@@ -1,11 +1,13 @@
 import logging
 import datetime
+import shutil
 
 from census_istat.config import logger, console_handler, MAIN_PATH, PREPROCESSING_FOLDER
 from census_istat.geodata.manage_geodata import join_year_census
 
 logger.addHandler(console_handler)
 target_years = [1991, 2001, 2011]
+delete_process_folder = True
 
 if __name__ == '__main__':
     time_start = datetime.datetime.now()
@@ -22,9 +24,13 @@ if __name__ == '__main__':
                 data_path=year_data,
                 year=year,
                 output_path=output_path,
-                remove_processed=True,
+                remove_processed=delete_process_folder,
                 only_shared=True
             )
+
+    if delete_process_folder:
+        logging.info(f'Delete data path {data_path}')
+        shutil.rmtree(data_path)
 
     time_end = datetime.datetime.now() - time_start
     logging.info(f'End analysis in {time_end}')
