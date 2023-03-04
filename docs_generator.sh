65a5657a5b88a0dfaba83a6d3bb1c4c9b86a4984
MaxDragonheart <massimilianomoraca@gmail.com>
2023-03-04 22:06:56 +0100

    feat github pages

--- docs_generator.sh
+++ docs_generator.sh
@@ -10,13 +10,14 @@ for MODULE_PATH in $MODULE_PATHS
 do
 # Generate source files from module path
 sphinx-apidoc -f -o $SOURCE_DIR $MODULE_PATH
+# make html
 done
 
 # Replace delimiter
 SOURCE_FILES=${MODULE_PATHS/ /\\n   }
 
 # Insert source file name
-sed "s/Contents:/Contents:\n\n   $SOURCE_FILES/g" $SOURCE_DIR/index.tmpl.rst > $SOURCE_DIR/index.rst
+# sed "s/Contents:/Contents:\n\n   $SOURCE_FILES/g" $SOURCE_DIR/index.tmpl.rst > $SOURCE_DIR/index.rst
 
 # Generate html documentation from source files
 sphinx-build -b html $SOURCE_DIR $BUILD_DIR
