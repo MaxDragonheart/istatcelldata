2f29fddd76bc53b8c98ab4d1cf944c08e03285d0
MaxDragonheart <massimilianomoraca@gmail.com>
2023-04-14 10:34:29 +0200

    feat minor upgrades

--- scripts/a_download_raw_data.py
+++ scripts/a_download_raw_data.py
@@ -1,22 +1,5 @@
-import logging
-import datetime
-
-from istatcelldata.config import logger, console_handler, MAIN_PATH
-from istatcelldata.download import download_all_census_data
-
-logger.addHandler(console_handler)
-target_years = [1991, 2001, 2011]
+from istatcelldata.config import MAIN_PATH
+from istatcelldata.processes import download_raw_data
 
 if __name__ == '__main__':
-    time_start = datetime.datetime.now()
-    logging.info(f'Start analysis at {time_start}')
-
-    for year in target_years:
-        logging.info(f'Start download census data for year {year}')
-        download_all_census_data(
-            output_data_folder=MAIN_PATH,
-            year=year
-        )
-
-    time_end = datetime.datetime.now() - time_start
-    logging.info(f'End analysis in {time_end}')
+    download_raw_data(MAIN_PATH)
--- scripts/b_process_raw_data.py
+++ scripts/b_process_raw_data.py
@@ -1,48 +1,5 @@
-import logging
-import datetime
-
-from istatcelldata.config import logger, console_handler, MAIN_PATH, DATA_FOLDER, PREPROCESSING_FOLDER, \
-    CENSUS_DATA_FOLDER
-from istatcelldata.data.census_1991_2001 import merge_data_1991_2001
-from istatcelldata.data.manage_data import merge_data
-from istatcelldata.geodata.manage_geodata import read_raw_census_geodata
-
-logger.addHandler(console_handler)
-target_years = [1991, 2001, 2011]
+from istatcelldata.config import MAIN_PATH
+from istatcelldata.processes import process_row_data
 
 if __name__ == '__main__':
-    time_start = datetime.datetime.now()
-    logging.info(f'Start analysis at {time_start}')
-    data_path = MAIN_PATH.joinpath(PREPROCESSING_FOLDER)
-
-    for year in target_years:
-        logging.info(f'Process data {year}')
-        csv_path = data_path.joinpath(f'census_{year}').joinpath(DATA_FOLDER).joinpath(CENSUS_DATA_FOLDER)
-
-        output_data_path = csv_path.parent.parent
-
-        if year in [1991, 2001]:
-            merge_data_1991_2001(
-                csv_path=csv_path,
-                year=year,
-                output_path=output_data_path
-            )
-            read_raw_census_geodata(
-                data_path=data_path,
-                year=year,
-                output_path=output_data_path
-            )
-        else:
-            merge_data(
-                csv_path=csv_path,
-                year=year,
-                output_path=output_data_path
-            )
-            read_raw_census_geodata(
-                data_path=data_path,
-                year=year,
-                output_path=output_data_path
-            )
-
-    time_end = datetime.datetime.now() - time_start
-    logging.info(f'End analysis in {time_end}')
+    process_row_data(MAIN_PATH)
