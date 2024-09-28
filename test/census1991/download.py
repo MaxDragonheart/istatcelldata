7e64b4e83ea29cafd65203d50cd11b61e4279dbd
Massimiliano Moraca <massimilianomoraca@gmail.com>
2024-09-28 17:33:50 +0200

    feat use dataset 2021 #39

--- test/census1991/download.py
+++ test/census1991/download.py
@@ -10,8 +10,8 @@ from istatcelldata.census1991.download import download_geodata, download_data, r
 def test_read_xls(tmp_path: Path):
     print('test_read_xls')
     data = read_xls(
-        file_path=DEMO_DATA_FOLDER.joinpath("R02_DatiCPA_1991.xls"),
-        #output_path=tmp_path,
+        file_path=DEMO_DATA_FOLDER.joinpath("dati-cpa_1991\R05_DatiCPA_1991.xls"),
+        output_path=tmp_path,
     )
     print(data)
     assert isinstance(data, Path) or isinstance(data, pd.DataFrame)
